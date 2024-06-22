b02788ed577e8bf4c54b37611c01dde1be8e42f6
Urjith Mishra <66270347+TheScientist101@users.noreply.github.com>
2024-06-22 13:42:32 -0500

    finished easel with the web interface

--- easel.js
+++ easel.js
@@ -3,6 +3,7 @@ import { Interpreter } from './interpreter.js';
 import stdlib, { EaselError } from './stdlib.js';
 import { Lexer } from './lexer.js';
 import { Parser } from './parser.js';
+import readline from 'node:readline';
 
 const readFile = location => new Promise((resolve, reject) => fs.readFile(location, 'utf-8', (err, data) => {
     if (err) return reject(err)
@@ -49,6 +50,53 @@ const writeFile = (location, data) => new Promise((resolve, reject) => fs.writeF
             console.log(err);
         }
     } else {
+        const interpreter = new Interpreter();
+        let scope = {
+            ...stdlib,
+            exit: () => process.exit(1)
+        };
+        const input = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
 
+        process.on('SIGINT', () => {
+            input.close();
+        });
+
+        const repl = line => {
+            let hadError = false;
+
+            const lexer = new Lexer(line);
+            try {
+                lexer.scanTokens();
+            } catch (err) {
+                if (err instanceof EaselError) {
+                    hadError = true;
+                    console.log(err.toString());
+                } else throw err;
+            }
+
+            if (!hadError) {
+                const parser = new Parser(lexer.tokens);
+                try {
+                    parser.parse();
+                } catch (err) {
+                    if (err instanceof EaselError) console.log(err.toString());
+                    else throw err;
+                }
+
+                try {
+                    interpreter.run(parser.ast, stdlib);
+                } catch (err) {
+                    if (err instanceof EaselError) console.log(err.toString());
+                    else throw err;
+                }
+            }
+
+            input.question('> ', repl);
+        }
+
+        input.question('> ', repl);
     }
 })()
